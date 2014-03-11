fd483eac5c86e53120734bf92cf222f1f442e1e6
Julie Soliman <julie.soliman@gmail.com>
2014-03-10 20:18:49 -0500

    Fixed topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find_by(id: params[:id])
-    @posts = Post.joins('INNER JOIN mentions ON posts.id = mentions.post_id').where('mentions.topic_id = ?', @topic.id)
+    @posts = @topic.posts
   end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
-  has_many :posts
+  has_many :mentions
+  has_many :posts, through: :mentions
 end
